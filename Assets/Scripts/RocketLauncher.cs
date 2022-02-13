1e98019657a6e4e81f67fcdeda08a1b983ae50a8
C Chen <58405541+damenakoi@users.noreply.github.com>
2022-02-13 02:07:38 -0600

    Knockback + rocket launcher

--- Assets/Scripts/RocketLauncher.cs
+++ Assets/Scripts/RocketLauncher.cs
@@ -5,23 +5,49 @@ using UnityEngine;
 public class RocketLauncher : MonoBehaviour
 {
     [SerializeField]
-    private GameObject bullet;
-    [SerializeField]
-    private GameObject explosion;
-    [SerializeField]
-    public static bool hasRL = false;
+    private GameObject rocket;
     [SerializeField]
     private bool coolingDown = false;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private double ShootTimer = 1; //Wait period before shooting
+    [SerializeField]
+    private static bool CanShoot = true;
+    [SerializeField]
+    private double _Tracker; //Tracks time
+
+    private Transform ShotPoint;
+    private GameObject temp;
+
     void Start()
     {
-        
+        _Tracker = ShootTimer; //Tracks time
+        temp = GameObject.Find("ShootPoint");
+        ShotPoint = temp.GetComponent<Transform>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        temp = GameObject.Find("ShootPoint");
+        ShotPoint = temp.GetComponent<Transform>();
+
+        ShootTimer += Time.deltaTime; //Updates time
+
+        if (ShootTimer > _Tracker) //When player can shoot
+        {
+            Shoot(); //shoots
+        }
+
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire2") && CanShoot == true)
+        {
+            ShootTimer = 0; //Resets timer
+            GameObject aRocket = Instantiate(rocket, ShotPoint.position, ShotPoint.rotation); //Spawns rocket
+            aRocket.GetComponent<Rigidbody2D>().velocity = 5 * transform.right;
+        }
     }
 }
