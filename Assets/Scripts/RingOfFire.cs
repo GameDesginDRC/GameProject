05bc52a8c33b2f2fbe651f225c793305b14a4f08
robertyang13 <91514950+robertyang13@users.noreply.github.com>
2022-02-18 12:51:48 -0600

    new stage

---
+++ Assets/Scripts/RingOfFire.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingOfFire : MonoBehaviour
+{
+    [SerializeField]
+    Transform rotationCenter;
+    [SerializeField]
+    float rotationradius = 10f, angularspeed = 4f, rotatespdz = 2f;
+
+    private Vector2 rotatespd;
+
+    float posX, posY, angle = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationradius;
+        posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationradius;
+        transform.position = new Vector2(posX, posY);
+        angle = angle + Time.deltaTime * angularspeed;
+        if (angle >= 360)
+        {
+            angle = 0f;
+        }
+    }
+}
